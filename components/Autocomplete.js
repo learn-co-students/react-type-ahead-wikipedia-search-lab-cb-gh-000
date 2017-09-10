87bfa3f4afbe48139e7b13e37ef382d67f365707
Dominik Kurbiel <github email address>
2017-09-10 19:43:54 +0200

    Done.

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -7,19 +7,46 @@ import resultStore from '../stores/resultStore';
 import SearchField from './SearchField';
 import SearchResults from './SearchResults';
 
-class Autocomplete extends React.Component {
+export default class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      query: '',
+      results: resultStore.getState().results,
     };
+		
+		this.handleChange = this.handleChange.bind(this);
   }
+	
+	componentDidMount() {
+		this.removeListener = resultStore.addListener(({results}) => {
+			this.setState({results});
+		});
+	}
+	
+	componentWillUnmount() {
+		this.removeListener();	
+	}
+	
+	handleChange(ev) {
+		const query = ev.target.value;
+		this.setState({query});
+		
+		if(query > 2) {
+			actions.search(query);
+		}
+	}
+	
+	
   render() {
     return (
-      <div>
+      <div className="autocomplete">
         <h2>Autocomplete</h2>
+			
+			<SearchField value={this.state.query} onChange={this.handleChange}/>
+			<SearchResults results={this.state.results} />
       </div>
     );
   }
 }
 
-export default Autocomplete;
