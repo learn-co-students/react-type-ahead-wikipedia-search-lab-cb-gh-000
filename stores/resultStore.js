ec23db72c3571d8a9d8d3055eda9265ef9827f27
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-09 13:56:41 +0000

    Done.

--- stores/resultStore.js
+++ stores/resultStore.js
@@ -2,9 +2,21 @@
 
 import Store from './Store';
 
-class ResultStore {
+class ResultStore extends Store {
+
+  isOutdated(date){
+    if (date < this.getState().updated){
+      return true;
+    }else {
+    return false;
+    }
+
+  }
 }
 
-const resultStore = new ResultStore();
+const resultStore = new ResultStore({
+  results: [],
+  updated: new Date(),
+});
 
 export default resultStore;
