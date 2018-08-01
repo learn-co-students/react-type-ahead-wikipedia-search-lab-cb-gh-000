1fe60c30f15a6afb45a900f5470ab66ac1e9b4de
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-08-01 14:09:10 +0000

    Wikipedia Search Lab

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -11,12 +11,39 @@ class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      results: resultStore.getState().results,
+      query: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+
+  componentDidMount() {
+    this.removeListener = resultStore.addListener(({ results }) => {
+      this.setState({ results });
+    });
+  }
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  handleChange(event){
+    const query = event.target.value;
+    this.setState({
+      query: query
+    })
+
+    if (query.length > 2){
+      actions.search(query)
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className='autocomplete'>
         <h2>Autocomplete</h2>
+        <SearchField value={this.state.query} onChange={this.handleChange}/>
+        <SearchResults results={this.state.results} />
       </div>
     );
   }
--- stores/Store.js
+++ stores/Store.js
@@ -1,13 +1,20 @@
 'use strict';
 
 export default class Store {
+
   constructor(initialState) {
     this.state = initialState;
     this.listeners = [];
   }
 
   addListener(listener) {
-    // TODO
+    this.listeners.push(listener);
+
+    const removeListener = () => {
+      this.listeners = this.listeners.filter(l => listener !== l);
+    }
+
+    return removeListener;
   }
 
   setState(state) {
